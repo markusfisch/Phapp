ab23551edb69e019761d4f0130c25d43cc3aba59
Markus Fisch <mf@markusfisch.de>
2013-06-24 22:43:53 +0200

    Moved samples into own branch
    So you can use Phapp as submodule or subtree without having those
    samples lying around.
    .

--- samples/hello-world/Hello.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Hello extends PhappView
-{
-	public function response()
-	{
-		return 'Hello World.';
-	}
-}
--- samples/hello-world/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once '../../Phapp.php';
-require_once 'Hello.php';
-
-$app = new Phapp();
-
-echo $app->process( 'Hello' );
